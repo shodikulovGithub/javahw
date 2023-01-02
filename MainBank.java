9841ff64571188db245695d3d2f1caee5dfed086
shodikulovGithub <62528391+shodikulovGithub@users.noreply.github.com>
2023-01-01 22:00:48 -0500

    Add files via upload

---
+++ MainBank.java
@@ -0,0 +1,11 @@
+package com.yearup.week4.homework.Banking_Application;
+
+public class MainBank {
+    public static void main(String[] args) {
+
+        Account abd = new Account("Abdulbari", "B00002");
+        abd.showMenu();
+
+
+    }
+}
